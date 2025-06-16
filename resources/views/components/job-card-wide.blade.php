e87437ea888f4c68f7778ce561c32cacff4a32d8
Fatma R <anthoschichequs@gmail.com>
2025-06-16 11:17:57 +0300

    final project chap2 tamamlandi

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,12 +1,12 @@
-<div class="p-4 bg-white/7 rounded-xl flex gap-x-6">
+<x-panel class="flex gap-x-6">
     <div>
-    <x-employer-logo />
+        <x-employer-logo/>
     </div>
 
     <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400">Laracats</a>
 
-        <h3 class="font-bold text-lg mt-2">Video Producer</h3>
+        <h3 class="font-bold text-lg mt-2 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
         <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
 
@@ -15,5 +15,6 @@
         <x-tag>Tag</x-tag>
         <x-tag>Tag</x-tag>
     </div>
+</x-panel>
 
-</div>
+
