72e25b6802de12fd99d81ff04d5eeac44b23d245
Fatma R <anthoschichequs@gmail.com>
2025-06-16 17:59:03 +0300

    final project chap4(son) tamamlandi.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <title>Final Project</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
-<body class="bg-black text-white">
+<body class="bg-black text-white pb-20">
 
 <div class="px-10">
     <nav class="flex justify-between items-center py-4 border-b border-white/10">
@@ -17,15 +17,31 @@
         </div>
 
         <div class="space-x-6 font-bold">
-            <a href="">Jobs</a>
-            <a href="">Carriers</a>
-            <a href="">Salaries</a>
-            <a href="">Companies</a>
+            <a href="/">Jobs</a>
+            <a href="#">Carriers</a>
+            <a href="#">Salaries</a>
+            <a href="#">Companies</a>
         </div>
 
-        <div>
-            <a href="">Post a Job</a>
-        </div>
+        @auth
+            <div class="space-x-6 font-bold flex">
+                <a href="/jobs/create">Post a Job</a>
+
+                <form method="POST" action="/logout">
+                    @csrf
+                    @method('DELETE')
+
+                    <button>Log Out</button>
+                </form>
+            </div>
+        @endauth
+
+        @guest
+            <div class="space-x-6 font-bold">
+                <a href="/register">Sign Up</a>
+                <a href="/login">Log In</a>
+            </div>
+        @endguest
     </nav>
 
     <main class="mt-10 max-w-[986px] mx-auto">
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,7 +1,7 @@
 @props(['tag', 'size' => 'base'])
 
 @php
-    $classes = "bg-white/10 hover:bg-white/25  rounded-xl  font-bold transition-colors duration-300";
+    $classes = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300";
 
     if ($size === 'base') {
         $classes .= " px-5 py-1 text-sm";
@@ -11,6 +11,4 @@
     }
 @endphp
 
-<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
-
-
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ ucwords($tag->name) }}</a>
